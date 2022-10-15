013e8abb3502cac9fbbdd0bc880d7f1ab264f548
Mauro Bassini <mbassini@outlook.com>
2022-10-15 16:00:28 -0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const getGeolocation = require('./utils/geolocation')
 const getForecast = require('./utils/forecast')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
